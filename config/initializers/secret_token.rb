d3b46a4be4f3741548a548d6f1e25b2946f6ede0
j3tm0t0 <j3tm0t0@gmail.com>
2013-07-30 12:41:15 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nicochu::Application.config.secret_token = '3ab200ca51fc402491e31c6c69f6f208e191f954f983a7ab5719d020ca057de11dc03a30647eb1994d9c14216d46d072ff69770c6d16a46a81dc7308ea6df093'
